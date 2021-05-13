2fdd207b12b77462e2e544a6184e41aa23dbd92b
ola <espadah@hotmail.com>
2021-05-14 00:41:48 +0200

    Login

---
+++ basic2/src/main/java/com/example/basic2/login/Security.java
@@ -0,0 +1,43 @@
+package com.example.basic2.login;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+
+@EnableWebSecurity
+public class Security extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select user_name,CONCAT('{noop}',password),true from users where user_name=?")
+        .authoritiesByUsernameQuery("select user_name,role from users where user_name=?");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .permitAll()
+                    .defaultSuccessUrl("/plank")
+                    .loginPage("/login")
+                .and()
+                .logout()
+                    .permitAll()
+                    .deleteCookies("JSESSIONID");
+
+
+    }
+
+}
