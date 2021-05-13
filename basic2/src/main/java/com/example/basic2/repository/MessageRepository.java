bc3314801d0946053e2b70247fcff06bd864a98e
ola <espadah@hotmail.com>
2021-05-14 00:43:39 +0200

    Message, User Repositories

---
+++ basic2/src/main/java/com/example/basic2/repository/MessageRepository.java
@@ -0,0 +1,9 @@
+package com.example.basic2.repository;
+
+import com.example.basic2.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+}
---
+++ basic2/src/main/java/com/example/basic2/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.basic2.repository;
+
+import com.example.basic2.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long> {
+    Users findByUserName(String userName);
+}
