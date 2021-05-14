89965bb0e6851989c33a4f86731114211a3bde8c
ola <espadah@hotmail.com>
2021-05-14 15:00:31 +0200

    UserID till meddelande

--- basic2/src/main/java/com/example/basic2/controller/MessageController.java
+++ basic2/src/main/java/com/example/basic2/controller/MessageController.java
@@ -3,13 +3,17 @@ import com.example.basic2.entity.Message;
 import com.example.basic2.entity.Users;
 import com.example.basic2.repository.UserRepository;
 import com.example.basic2.service.MessageService;
+import com.example.basic2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import javax.swing.*;
 import java.security.Principal;
+
+import static org.springframework.data.domain.Sort.Direction.ASC;
 
 @Controller
 public class MessageController {
@@ -19,14 +23,17 @@ public class MessageController {
 
     @Autowired
     private UserRepository userRepository;
+    private UserService userService;
 
     @GetMapping("/plank")
     public String plank(@ModelAttribute("mssg")Message mess, Model model, Principal principal) {
 
-        model.addAttribute("msg", messageService.getAllMessages());
+        model.addAttribute("user", userRepository);
+        model.addAttribute("msgg", messageService.getAllMessages());
         model.addAttribute("name", principal.getName());
         return "plank";
     }
+
 
     @GetMapping("/saveMessage")
     public String saveMessage(@ModelAttribute("msg") Message msg, Principal principal){
@@ -39,6 +46,6 @@ public class MessageController {
 
     @GetMapping("/logout")
     public String logout(){
-        return "redirect:/plank";
+        return "redirect:/";
     }
 }
